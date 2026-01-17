570d7e5d68901eb6c4b63ae84f4f1f4cccb6b7c9
wafaamsawan <wafaamsawan@gmail.com>
2026-01-17 20:16:12 +0200

    update base

--- routes/base.py
+++ routes/base.py
@@ -8,9 +8,9 @@ base_router = APIRouter(
 )
 
 @base_router.get("/") 
-def welcome():
-    app_name=os.getenv('APP_NAME')
-    app_version=os.getenv('APP_VERSION')
+async def welcome():
+    app_name = os.getenv('APP_NAME')
+    app_version = os.getenv('APP_VERSION')
     return{
         "app_name":app_name,
         "app_version":app_version
